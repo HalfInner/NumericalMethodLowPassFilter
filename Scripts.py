4f4398a056d66ef743b22921f264b8298bda7868
Half's Inner <kajbrz@hotmail.com>
2019-11-16 23:16:59 +0100

    Find the Transmit Function

--- Scripts.py
+++ Scripts.py
@@ -3,6 +3,8 @@ from decimal import Decimal
 # import control as control
 import matplotlib.pyplot as plt
 import numpy as np
+from numpy import transpose
+from scipy import signal
 
 from numpy.ma import arange
 
@@ -25,7 +27,7 @@ du1/dt  = 1/C1(1/
 
 '''
 
-''' 
+'''
 G(s)=\dfrac{V_{in}}{V_{out}}=
 \dfrac{
     \dfrac{1}{
@@ -38,54 +40,52 @@ G(s)=\dfrac{V_{in}}{V_{out}}=
 '''
 
 
-def G_s(R1: float, R2: float, C1: float, C2: float, S: float):
-    V_out = 1. / (R1 * R2 * C1 * C2)
-    V_in = S ** 2 + S * (1 / (R1 * C1) + 1 / (R2 * C1) + 1 / (R2 * C2)) + 1 / (R1 * R2 * C2 * C1)
-    return V_out / V_in
+def G_s(R1: float, R2: float, C1: float, C2: float):
+    V_out_ss = 0.
+    V_out_s = 0.
+    V_out_c = 1. / (R1 * R2 * C1 * C2)
+
+    V_in_ss = 0.
+    V_in_s = 1. / (R1 * C1) + 1. / (R2 * C1) + 1. / (R2 * C2)
+    V_in_c = 1. / (R1 * R2 * C1 * C2)
+    return signal.TransferFunction(
+        [V_out_ss,  V_out_s, V_out_c],
+        [V_in_ss,   V_in_s,  V_in_c])
+
 
 
 def main():
     # Data:
     R1 = 1 * 1000
-    C1 = 0.032 * 1e6
+    C1 = 0.032 * 1e3
     R2 = 1 * 1000
-    C2 = 0.032 * 1e6
+    C2 = 0.032 * 1e3
     RL = 1 * 1000
 
     time = 1
     step = 0.01
+    e_t = 2.
     time_vector = np.arange(start=0, stop=time, step=step, dtype=float)
-    f = 10
-    value_vector = np.sin(time_vector * 2 * np.pi * f)
+    time_vector = np.linspace(start=0, stop=1)
+    input_const_vec = np.ones(len(time_vector)) * e_t
 
-    const = 0
-    f = const
-    transfer_function = G_s(R1, R2, C1, C2, f)
-    e_t = 2
-    voltage_input_vector = [e_t]*len(time_vector)
+    system = G_s(R1, R2, C1, C2)
+    _, out_vec, v = signal.lsim(system, input_const_vec, time_vector)
 
-    output_vector = []
-    u_1 = 0.
-    u_2 = 0.
-    for t in time_vector:
-        idx = int(t / step)
-        du1_dt = (1. / C1) * ((1. / R1) * (e_t - u_1) - (1. / R2) * (u_1 - u_2))
-        du2_dt = (1. / C2) * ((1. / R2) * (u_1 - u_2) - (u_2 / 1.e30))
-        u_1 = du1_dt * t
-        u_2 = du2_dt * t
-        output_vector.append(u_2 * e_t)
-        print("[{:5.5}: {:6.5};{:6.5} => {}]".format(t, u_1, u_2, output_vector[idx]))
-        # print(output_vector)
+    plt.figure(0)
+    plt.plot(time_vector, input_const_vec, label='Input f=0')
+    plt.plot(time_vector, out_vec, label='Response f=0')
 
-    plt.plot(time_vector, voltage_input_vector, label='Input Voltage')
-    # plt.plot(time_vector, value_vector)
-    plt.plot(time_vector, np.array(output_vector), label='Output Voltage')
-    plt.ylim(bottom=0, top=e_t*1.1)
+    plt.ylim(bottom=-2., top=e_t*1.1)
     plt.xlabel('[s]')
     plt.ylabel('[V]')
     plt.legend()
     plt.show()
 
+    w, mag, phase = signal.bode(system)
+    plt.figure(1)
+    plt.plot(w, phase)
 
+    plt.show()
 if __name__ == "__main__":
     main()
